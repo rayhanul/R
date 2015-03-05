a15b398e1fde68be4ed9545407d7e37a3230b7a4
rayhanul <rayhan.iit@gmail.com>
2015-03-05 21:12:51 +0600

    adding local vs global by tim menzies

---
+++ TimMenzies.R
@@ -0,0 +1,143 @@
+source('C:/Users/x-man/Copy/R/Defect_Prediction/datasetManager.R')
+source('C:/Users/x-man/Copy/R/Defect_Prediction/FileManager.R')
+source('C:/Users/x-man/Copy/R/Defect_Prediction/DataInfo.R')
+
+data<-read.csv(sourcePath["mainFile"])
+
+splittedData<-splitData(data)
+
+
+getDimentionReducedData<-function(splitteTrainData){
+
+		fit<-princomp(splitteTrainData[3:10],cor = TRUE)
+		dimReducedData<-cbind(fit$scores[,1],fit$scores[,2])
+	return(dimReducedData)
+}
+
+#getlargestAndSmallestItem<-function(dimReducedData){
+
+	#	for ( 1 in 0: length(dimReducedData)){
+			#item<-dimReducedData[i,]
+			#dis<- sqrt((item[,1])^2+(item[,2])^2)
+			#dimReducedData[i,1]<-cbind(item,dis)
+		#}
+		#smallest<-tail(sort(dimReducedData[,3]),1)
+
+		#largest<-head(sort(dimReducedData[,3]),1)
+		
+		
+	#return(list(largest=largest,smallest=smallest))
+
+#}
+
+
+getFirstQuadrantPosition<-function(dimReducedData){
+
+#largestAndSmallest<-getlargestAndSmallestItem(splittedData$trainset)
+
+x<-median(dimReducedData[,1])
+	y<-median(dimReducedData[,2])
+	
+	return(list(x=x,y=y))
+
+}
+
+
+divideDataIntoQuadrant<-function(dataTobeReduced){
+
+	dimReducedData<-getDimentionReducedData(dataTobeReduced)
+	position<-getFirstQuadrantPosition(dimReducedData)
+	newdimReducedData<-c()
+		for(i in 1: nrow(dimReducedData)){
+			rowItem<-dimReducedData[i,]
+			#...first quadrant....
+			if(rowItem[1]>position["x"] & rowItem[2]>position["y"]){
+		  
+			  rowItem[3]<- 1
+			}
+			
+			
+			#...secend quadrant....
+			if(rowItem[1]>position["x"] & rowItem[2]<position["y"]){
+			 # newItem<-cbind(rowItem,c(2))
+			  rowItem[3]<- 2
+			}
+			
+			#...3rd quadrant....
+			if(rowItem[1]<=position["x"] & rowItem[2]<=position["y"]){
+			  #newItem<-cbind(rowItem,c(3))
+			  rowItem[3]<-3
+			}
+			
+			#... 4th quadrant....
+			if(rowItem[1]<position["x"] & rowItem[2]>position["y"]){
+			 # newItem<-cbind(rowItem,c(4))
+			 rowItem[3]<-4
+			}
+		
+			newdimReducedData<-rbind(newdimReducedData, rowItem)
+		}
+return(newdimReducedData)
+}
+
+getPredictedCluster<-function(testData){
+		predictedClusters<- divideDataIntoQuadrant(testData)
+	return(predictedClusters)
+}
+
+getRealClusteredDataForTrainPredictionModel<-function(dimReducedDataWithClusterInfo, clusterId){
+
+	#reducedClusterData<-dimReducedDataWithClusterInfo[dimReducedDataWithClusterInfo[,3]==i]
+	trainData<-c()
+	for( i in 1: nrow(dimReducedDataWithClusterInfo)){
+		newItem<-dimReducedDataWithClusterInfo[i,]
+		
+		if(newItem[3]==clusterId){
+			d<-splittedData$trainset[i,]
+			trainData<-rbind(trainData,d )
+		}
+	}
+  
+  return(trainData)
+}
+
+getRealClusteredDataForTestPredictionModel<-function(dimReducedDataWithClusterInfo, clusterId){
+
+	#reducedClusterData<-dimReducedDataWithClusterInfo[dimReducedDataWithClusterInfo[,3]==i]
+	testData<-c()
+	for( i in 1: nrow(dimReducedDataWithClusterInfo)){
+		newItem<-dimReducedDataWithClusterInfo[i,]
+		
+		if(newItem[3]==clusterId){
+			d<-splittedData$testset[i,]
+			testData<-rbind(testData,d )
+		}
+	}
+  
+  return(testData)
+}
+getPredictedDefect<-function(){
+
+	#dimReducedData<-getDimentionReducedData(splittedData$trainset) 
+	#position<-getFirstQuadrantPosition(dimReducedData)
+	dimReducedDataWithClusterInfo<-divideDataIntoQuadrant(splittedData$trainset)
+	
+	
+	dimReducedpredictedClustersList<-getPredictedCluster(splittedData$testset)
+
+  numberOfClusters<-tail(sort(dimReducedpredictedClustersList[,3]),1)
+  predictedDefects<-c()
+  for(clster in 1: numberOfClusters){
+  
+  mainTrainData<-getRealClusteredDataForTrainPredictionModel(dimReducedDataWithClusterInfo,clster)
+  testData<-getRealClusteredDataForTestPredictionModel(dimReducedpredictedClustersList,clster)
+  
+ model<-lm(bug~wmc+loc+npm+cbo+lcom+rfc+noc+dit,mainTrainData)
+ pDefect<-predict(model, testData,interval="predict")
+ #dataFrame<-pDefect.frame()
+ predictedDefects<-rbind(predictedDefects,pDefect)
+  }
+  #writeDataFrameTo(predictedDefects,sourcePath[['PcaAnalysisPath']],"residualTimMenzies")
+ getCombinedTestDataWithResidualValues(splittedData$testset,predictedDefects,"testDataWithResidualValuesForMenzies.csv")
+ return(predictedDefects)
+}
