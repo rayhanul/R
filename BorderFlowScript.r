78ac86dff93ec4507b2116b9d37ddcd5d83e3b58
rayhanul <rayhan.iit@gmail.com>
2015-03-15 00:45:47 +0600

    implementing borderflow algorithm

--- BorderFlowScript.r
+++ BorderFlowScript.r
@@ -16,6 +16,25 @@ source('C:/Users/x-man/Copy/R/Defect_Prediction/DataInfo.R')
 	#list(trainset=train, testset=test)
 #}
 
+# predictDefectUsingBorderFlow<-function(){
+# 
+# 	allFiles<-list.files(sourcePath[['BorderFlowfilePath']])
+# 	combindedData<-c()
+# 		for(i in 1: length(allFiles)){
+# 		combindedPath<-paste(sourcePath[['BorderFlowfilePath']],allFiles[[i]],sep="/")
+# 			data<-read.csv(combindedPath)
+# 			splittedData<-splitData(data)
+# 			predictedDefect<-getPredictedDefects(splittedData$trainset,splittedData$testset)	
+# 			combindedData<-rbind(combindedData,predictedDefect)
+# 		}
+# 	writeDataFrameTo(combindedData,sourcePath[['BorderFlowfilePath']],"RESIDUAL")
+# return(combindedData)
+# }
+
+data<-read.csv(sourcePath[['mainFile']],stringsAsFactors=FALSE)
+splittedData<-splitData(data)
+
+
 getPredictedDefects<-function(trainData,testData){
 
 	predictionModel<-lm(bug~wmc+loc+npm+cbo+lcom+rfc+noc+dit,trainData)
@@ -23,17 +42,39 @@ getPredictedDefects<-function(trainData,testData){
 	return(predictedDefect)
 }
 
+getDataForClusterId<-function(mainData,clusterInfoList, clusterId){
+  clusterTrainingData<-c()
+	for(i in 1: nrow(clusterInfoList)){
+		clusterInfoListRowItem<-clusterInfoList[i,]
+		if(clusterInfoListRowItem[["jointClusterId"]]==clusterId){
+			for(j in 1:nrow(mainData)){
+				mainDataRowItem<-mainData[j,]
+				a=mainDataRowItem["name"]
+				b=clusterInfoListRowItem["name"]
+				if(a == b){
+					clusterTrainingData<-rbind(clusterTrainingData,mainDataRowItem)
+				}
+			}
+		}
+	}
+ return(clusterTrainingData)
+}
+
 predictDefectUsingBorderFlow<-function(){
 
-	allFiles<-list.files(sourcePath[['BorderFlowfilePath']])
+	clustersInfo<-read.csv(sourcePath[['BorderFlowClusterInfofile']],stringsAsFactors=FALSE)
+	numberOfClusters<-unique(clustersInfo[,2])
+	
 	combindedData<-c()
-		for(i in 1: length(allFiles)){
-		combindedPath<-paste(sourcePath[['BorderFlowfilePath']],allFiles[[i]],sep="/")
-			data<-read.csv(combindedPath)
-			splittedData<-splitData(data)
-			predictedDefect<-getPredictedDefects(splittedData$trainset,splittedData$testset)	
+		for(i in 1: length(numberOfClusters) ){		
+			trainingData<-getDataForClusterId(splittedData$trainset,clustersInfo,numberOfClusters[i])
+			testData<-getDataForClusterId(splittedData$testset,clustersInfo,numberOfClusters[i])
+			predictedDefect<-getPredictedDefects(trainingData,testData)	
 			combindedData<-rbind(combindedData,predictedDefect)
 		}
-	writeDataFrameTo(combindedData,sourcePath[['BorderFlowfilePath']],"RESIDUAL")
+		
+	writeDataFrameTo(combindedData,sourcePath[['dataPath']],"ResidualValForBorderFlow.csv")
 return(combindedData)
 }
+
+
